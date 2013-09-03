578b91aa647ef0c0fa0d34a98a3905db5dd574d6
Hamed Al-Khabaz <hamedalkhabaz@gmail.com>
2013-09-03 17:25:40 -0400

    fixing server

--- blog.php
+++
@@ -1,6 +0,0 @@
-<?php
-  header('Content-Type: application/javascript');
-  header("Access-Control-Allow-Origin: *");
-  $json = file_get_contents("http://api.outpost.travel:8706/");
-  echo "$json";
-?>
